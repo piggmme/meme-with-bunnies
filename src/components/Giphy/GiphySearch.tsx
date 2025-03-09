0bec5e0a2d916790a8fa1008b4ae5e5005d1da95
cherry.a <cherry.a@kakaocorp.com>
2025-03-09 15:24:31 +0900

    feat: 이미지 클릭시 캔버스에 추가되도록 변경

--- src/components/Giphy/GiphySearch.tsx
+++ src/components/Giphy/GiphySearch.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { fetchGifs } from '@/service/giphy'
 import type { GiphyGif } from '@/types/giphy'
+import { $editorImages } from '@/stores/editorState'
 
 export default function GiphySearch () {
   const [query, setQuery] = useState('')
@@ -28,6 +29,20 @@ export default function GiphySearch () {
             src={gif.images.fixed_height.url}
             alt={gif.title}
             style={{ width: '150px', margin: '5px' }}
+            onClick={() => {
+              $editorImages.set([
+                ...$editorImages.get(),
+                {
+                  type: 'gif',
+                  id: gif.id,
+                  src: gif.images.original.url,
+                  size: {
+                    width: Number(gif.images.original.width),
+                    height: Number(gif.images.original.height),
+                  },
+                },
+              ])
+            }}
           />
         ))}
       </div>
