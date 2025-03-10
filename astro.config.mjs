455570dd858194ac132dec73260a6a5387412d35
cherry.a <cherry.a@kakaocorp.com>
2025-03-10 20:46:42 +0900

    feat: Meme With Bunnies 로 변경

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: 'server',
+  adapter: cloudflare(),
   vite: {
     optimizeDeps: {
       exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
