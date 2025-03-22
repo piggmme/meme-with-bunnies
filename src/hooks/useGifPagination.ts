cb97f3b0e3b523387a0376d43decfd8813dcff72
dev_hee <kheeyaa888@gmail.com>
2025-03-22 16:42:11 +0900

    feat: sticker gif로 변경

--- src/hooks/useGifPagination.ts
+++ src/hooks/useGifPagination.ts
@@ -1,11 +1,11 @@
 import type { GiphyGif } from '@/types/giphy'
 import type { PaginationOptions } from '@/hooks/usePagination'
 import usePagination from '@/hooks/usePagination'
-import { fetchGifs } from '@/service/giphy'
+import { fetchStickers } from '@/service/giphy'
 
 export default function useGifPagination () {
   return usePagination<GiphyGif>(async (option: PaginationOptions) => {
-    const response = await fetchGifs(option)
+    const response = await fetchStickers(option)
     return response?.data
   })
 }
--- src/service/giphy.ts
+++ src/service/giphy.ts
@@ -6,5 +6,12 @@ export const fetchGifs = async (options: PaginationOptions) => {
   const { query, limit = 10, offset = 0 } = options
   if (!query) return
 
-  return await axios.get<GiphyGif[]>(`/api/gifs?q=${encodeURIComponent(query)}&limit=${limit}&offset=${offset}`)
+  return await axios.get<GiphyGif[]>(`/api/gifs/search?q=${encodeURIComponent(query)}&limit=${limit}&offset=${offset}`)
+}
+
+export const fetchStickers = async (options: PaginationOptions) => {
+  const { query, limit = 10, offset = 0 } = options
+  if (!query) return
+
+  return await axios.get<GiphyGif[]>(`/api/stickers/search?q=${encodeURIComponent(query)}&limit=${limit}&offset=${offset}`)
 }
